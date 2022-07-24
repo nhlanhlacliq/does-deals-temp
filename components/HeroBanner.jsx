b5c14d95ed15a70039d016226ec3b7bd03ecb3d2
nhlanhlacliq <nhlanhlacliq@gmail.com>
2022-07-24 12:08:11 +0200

    update

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -13,7 +13,7 @@ const HeroBanner = ({ heroBanner }) => {
             <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image' />
 
             <div>
-                <Link href={`/deal/${heroBanner.product}`}>
+                <Link href={`/product/${heroBanner.product}`}>
                     <button type='button'>{heroBanner.buttonText}</button>
                 </Link>
                 <div className='desc' >
@@ -23,8 +23,6 @@ const HeroBanner = ({ heroBanner }) => {
 
             </div>
         </div>
-        // HeroBanner
-
     </div>
   )
 }
