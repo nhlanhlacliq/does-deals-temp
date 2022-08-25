6a5b0784ee48c7e7401d4736ac20e6d2f4df2b0e
nhlanhlacliq <nhlanhlacliq@gmail.com>
2022-08-25 18:26:14 +0200

    modal attempt

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
 import { AiOutlineUser, AiOutlineShopping } from 'react-icons/ai';
-
+import { Modal } from '../components';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [ modalOpen, setModalOpen ] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen)
+  }
+
   return (
     <div className='navbar-container' >
       <p className='logo' >
         <Link href='/' > Doesdeals </Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick={() => alert('User menu under construction...')} >
+      <button type='button' className='cart-icon' onClick={() => toggleModal()} >
         <AiOutlineUser />
-        {/* <span className='cart-item-qty'>1</span> */}
       </button>
+
+      {modalOpen && <Modal closeModal={() => setModalOpen() }/>}
+
     </div>
   )
 }
