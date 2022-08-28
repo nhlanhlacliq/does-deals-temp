6737004bd2eda8f3023a743590edee80362c1b19
nhlanhlacliq <nhlanhlacliq@gmail.com>
2022-08-28 19:39:51 +0200

    update

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -9,7 +9,8 @@ function Modal({ closeModal }) {
         <div className="modal-container">
             <div className="modal-close">
                 <button className="modal-close-button" onClick={() => closeModal()}>
-                    {/* <AiOutlineCloseCircle size={25} /> */}
+                    {/* <AiOutlineCloseCircle size={25} />
+                     */}
                     <AiOutlineClose size={25} />
                 </button>
             </div>
