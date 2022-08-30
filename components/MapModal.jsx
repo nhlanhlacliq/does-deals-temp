b42c3d47ad88f3647048bfd9e39e2818fbe2a85e
nhlanhlacliq <nhlanhlacliq@gmail.com>
2022-08-30 17:06:05 +0200

    update to use address

--- components/MapModal.jsx
+++ components/MapModal.jsx
@@ -3,9 +3,9 @@ import { AiOutlineClose } from 'react-icons/ai';
 import { mainColor } from '../utils/styles';
 
 function MapModal({ closeModal, deal }) {
-    const { lat, lng } = deal.restaurant.area.location;
+    const address = deal.restaurant.area.address;
     const hexMainColor = '0x' + mainColor.slice(1).toUpperCase();
-    const mapUrl = `http://maps.google.com/maps/place/${lat},${lng}`
+    const mapUrl = `http://maps.google.com/maps/search/${address}`
 
     return (
         <div className='modal-background' >
@@ -21,13 +21,13 @@ function MapModal({ closeModal, deal }) {
                 <div className="modal-body">
                     <a href={mapUrl} >
                         <img 
-                            src={`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=15&size=400x350&markers=color:${hexMainColor}|label:${deal.restaurant.name.charAt(0)}|${lat},${lng}&key=AIzaSyDhgYqJh_AjhayFv58WXCYFa_8RK1LcWhk`}
+                            src={`https://maps.googleapis.com/maps/api/staticmap?center=${address}&zoom=15&size=400x350&markers=color:${hexMainColor}|label:${deal.restaurant.name.charAt(0)}|${address}&region=ZA&key=AIzaSyDhgYqJh_AjhayFv58WXCYFa_8RK1LcWhk`}
                         />
                     </a>
                 </div>
                 <div className="modal-footer">
                     <a href={mapUrl} >
-                        <button className='confirm-button' onClick={() => closeModal()} >View on Map</button>
+                        <button className='confirm-button' onClick={() => closeModal()} >View on Maps</button>
                     </a>
                     <button onClick={() => closeModal()} >Close</button>
                 </div>
