abfea45d550969facea6c29c89545d4bc61907fe
nhlanhlacliq <nhlanhlacliq@gmail.com>
2022-08-30 17:06:25 +0200

    update to use address

--- sanity_doesdeals/schemas/location.js
+++ sanity_doesdeals/schemas/location.js
@@ -9,20 +9,23 @@ export default {
     fields: [
         {
             name: 'area',
-            title: 'Area/Location',
+            title: 'Area',
             type: 'string',
             validation: Rule => Rule.required().warning(),
+            description: 'General area name, shown next to deal.'
         },
         {
-            name: 'location',
-            title: '(GoogleMap) Area/Location',
-            type: 'geopoint',
-        },
-        {
-            name: 'details',
-            title: 'Details',
-            description: 'Any extra details on the location',
-            type: 'text',
+            name: 'address',
+            title: 'Address',
+            type: 'string',
+            validation: Rule => Rule.required().warning(),
+            description: 'Specific address, shown on Google maps. E.g. \"Food Lover\'s Market Willowbridge, Belville\". Use the restaurant\'s name if a deal is National or similar - This is the search term Google maps will need.',
         }
-    ]
+    ],
+    preview: {
+        select: {
+          title: 'area',
+          subtitle: 'address'
+        }
+    }
 }
