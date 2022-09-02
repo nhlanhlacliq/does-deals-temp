ae31f088fbeea84a236b3ae829225822672c7292
nhlanhlacliq <nhlanhlacliq@gmail.com>
2022-09-02 15:46:47 +0200

    implement screen size based changes to modal width

--- components/MapModal.jsx
+++ components/MapModal.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai';
 import { mainColor } from '../utils/styles';
+import { useWindowDimensions } from '../utils/hooks';
 
 function MapModal({ closeModal, deal }) {
     const address = deal.restaurant.area.address;
     const hexMainColor = '0x' + mainColor.slice(1).toUpperCase();
     const mapUrl = `http://maps.google.com/maps/search/${address}`
+    const { height, width } = useWindowDimensions();
+
+    const mobile = width < 600 ? true : false;
+    const { mapWidth, mapHeight } = {
+        mapWidth: width < 600 ? 300 : 400,
+        mapHeight: 350
+    }
 
     return (
         <div className='modal-background' >
-            <div className="modal-container map-modal-container">
-                <div className="modal-close">
-                    <button className="modal-close-button" onClick={() => closeModal()}>
-                        <AiOutlineClose size={25} />
-                    </button>
-                </div>
+            <div className={`modal-container map-modal-container ${mobile && 'mobile'} `}>
                 <div className="modal-title map-modal-title">
                     {deal.restaurant.name}
                 </div>
                 <div className="modal-body">
                     <a href={mapUrl} >
                         <img 
-                            src={`https://maps.googleapis.com/maps/api/staticmap?center=${address}&zoom=15&size=400x350&markers=color:${hexMainColor}|label:${deal.restaurant.name.charAt(0)}|${address}&region=ZA&key=AIzaSyDhgYqJh_AjhayFv58WXCYFa_8RK1LcWhk`}
+                            src={`https://maps.googleapis.com/maps/api/staticmap?center=${address}&zoom=15&size=${mapWidth}x${mapHeight}&markers=color:${hexMainColor}|label:${deal.restaurant.name.charAt(0)}|${address}&region=ZA&key=AIzaSyDhgYqJh_AjhayFv58WXCYFa_8RK1LcWhk`}
                         />
                     </a>
                 </div>
